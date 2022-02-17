126ccd952594977f0041e25277a908f5eeb430ef
Higashimoto Kanami <summerwave5959@gmail.com>
2022-02-17 12:21:45 +0000

    [Start] public_home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :public do
+
+  end
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
@@ -10,12 +13,14 @@ Rails.application.routes.draw do
 
 
   scope module: :public do
+    root to: 'homes#top'
+    get '/about' => 'homes#about'
     resources :customers
-
   end
 
   namespace :admin do
     resources :admins
+
 
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
