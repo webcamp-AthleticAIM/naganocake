0cb7c4b28f3b125e42b48fee3045c99fe446274a
EC2 Default User <ec2-user@ip-172-31-91-106.ec2.internal>
2022-02-17 12:07:22 +0000

    [add] genre レイアウト未完成

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :admins
-
+    resources :genres, only: [:index, :create, :edit, :update]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
