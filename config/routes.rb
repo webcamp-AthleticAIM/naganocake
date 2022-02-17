ff90f409dfd9d5e6d81796f87e85dcb690155f43
EC2 Default User <ec2-user@ip-172-31-91-106.ec2.internal>
2022-02-17 06:31:30 +0000

    [fix] devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :customers
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
 
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
 
-  devise_for :admin
 
   #scope module: :public do
 
--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 2022_02_17_050629) do
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "last_name"
+    t.string "first_name"
+    t.string "last_name_kana"
+    t.string "first_name_kana"
+    t.string "postal_code"
+    t.string "address"
+    t.string "telephone_number"
+    t.boolean "is_deleted"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
