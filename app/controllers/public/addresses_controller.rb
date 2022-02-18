7e3bee409e6fa37affb807c0208b827656897e7a
Higashimoto Kanami <summerwave5959@gmail.com>
2022-02-18 13:42:52 +0000

    [Add] 配送先登録/一覧画面

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,19 +1,37 @@
 class Public::AddressesController < ApplicationController
+
   def create
     @address = Address.new(address_params)
-    if @address.save
-      redirect_to :index
+    @address.save
+      redirect_to address_path
   end
 
   def index
+    @addresses = Address.all
+    @address = Address.new
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to address_path
+    else
+      render edit_address_path
+    end
   end
 
   def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+  end
+
+  private
+
+  def address_params
+    params.riquer(:address).permit(:customer_id, :postal_code, :address, :name)
   end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,8 +1,5 @@
 class Address < ApplicationRecord
-<<<<<<< HEAD
    validates :postal_code, presence: true
    validates :address, presence: true
    validates :name, presence: true
-=======
->>>>>>> origin/develop
 end
