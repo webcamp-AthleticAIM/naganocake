e45eef523f64ec0ae1bb548cf5ddb187a794e1d1
Higashimoto Kanami <summerwave5959@gmail.com>
2022-02-19 05:06:23 +0000

    [Start] 会員情報一覧・詳細・変更機能

---
+++ app/controllers/admin/customers_controller.rb
@@ -0,0 +1,24 @@
+class Admin::CustomersController < ApplicationController
+  def index
+    @customers = Customer.all
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    @customer.save
+  end
+
+  private
+  def customer_params
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
