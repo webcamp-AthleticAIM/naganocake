8b3b9bd6ef640dce190cb52fef4f9d88ded353a5
Higashimoto Kanami <summerwave5959@gmail.com>
2022-02-19 03:40:56 +0000

    [Fix] 配送先編集画面一部修正

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,10 @@
 class Public::HomesController < ApplicationController
   def top
-    #@items = Item.limit(6).order('created_at DESC')
+    #@items = Items.limit(6).order('created_at DESC')
+  end
+
+  def index
+   #@items = Item.limit(6).order('created_at DESC')
   end
 
   def about
