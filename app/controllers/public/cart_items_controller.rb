0d690edd5066b88103a6c9229cfe00b79d51fd09
EC2 Default User <ec2-user@ip-172-31-3-84.ec2.internal>
2022-02-19 09:30:30 +0000

    [Add] カート機能検証のため、商品機能を簡易的に追加

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,8 +1,10 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = CartItem.all
   end
 
   def create
+
   end
 
   def update
@@ -12,5 +14,11 @@ class Public::CartItemsController < ApplicationController
   end
 
   def destroy_all
+  end
+
+  private
+
+  def cart_items_params
+    params.require(:cart_item).permit(:item_id, :item_amount)
   end
 end
