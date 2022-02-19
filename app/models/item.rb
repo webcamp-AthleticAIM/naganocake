c5db767e6249ec8dabcc7f653c6dba461ebad675
miya24n5 <sho.sakulai10@gmail.com>
2022-02-19 02:15:13 +0000

    [Finish]商品新規登録機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  validates :name, :price, :image, :introduction,  presence: true
+
+  # has_many:cart_items, dependent: :destroy
+  # has_many:order_details, dependent: :destroy
+  # belongs_to:genre
+
+  attachment :image
 end
