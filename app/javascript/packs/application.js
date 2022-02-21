03d787692589a56c708a41fec12331adcd02d767
Higashimoto Kanami <summerwave5959@gmail.com>
2022-02-21 23:46:47 +0000

    [Add] 会員情報一覧・詳細・編集レイアウト

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ import "bootstrap";
 import "../stylesheets/application";
 import '@fortawesome/fontawesome-free/js/all';
 import '../stylesheets/homes';
+import '../stylesheets/customers';
 
 Rails.start()
 Turbolinks.start()
