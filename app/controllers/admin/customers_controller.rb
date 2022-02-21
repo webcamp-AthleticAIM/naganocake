a407ceb772c47d5b28862f93965a07a23ece603e
Higashimoto Kanami <summerwave5959@gmail.com>
2022-02-21 05:27:58 +0000

    [Fix]customers_controller.rb修正

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -17,7 +17,7 @@ class Admin::CustomersController < ApplicationController
   def update
     @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
-      redirect_to admin_customer_paht(@customer)
+      redirect_to admin_customer_path(@customer)
     else
       render 'edit'
     end
