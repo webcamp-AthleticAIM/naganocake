a3e5387bfb108878fc654785233993e220dac928
EC2 Default User <ec2-user@ip-172-31-91-106.ec2.internal>
2022-02-22 02:49:38 +0000

    コントローラーエラー未処理

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,28 +4,40 @@ class Public::OrdersController < ApplicationController
     @order = Order.new
     @order.customer_id = current_customer.id
     @current_addresses = Customer.select(:postal_code, :address, :last_name, :first_name)
-    @order.name = current_customer.first_name + current_customer.last_name
-    
+    @order.name = current_customer.last_name + current_customer.first_name
+
   end
 
   def confirm
-    p @order = Order.new(order_params)
-    @order.postal_code = current_customer.postal_code
-    @order.address = current_customer.address
-    @order.name = current_customer.first_name + current_customer.last_name
-    @cart_items = CartItem.all
-  end
-
-  def create
-
-    p @order = Order.find(order_params)
-
-
-    if @order.sa
+    @order = Order.new(order_params)
+    #@cart_items = CartItem.find(params[:id])
+    if params[:order_addresses] == '0'
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.first_name + current_customer.last_name
+    elsif params[:order_addresses] == '1'
+      @order = Order.new(order_params)
+      @address = Address.find(params[:id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    elsif params[:order_addresses] == '2' && @order.postal_code? && @order.address? && @order.name?
 
     else
       render 'new'
     end
+  end
+
+  def create
+    @order = Order.find(order_params)
+
+    if @order.save
+      current_customer.cart_items.destroy_all
+      redirect_to 'thanks'
+    else
+      render 'new'
+    end
+
   end
 
   def thanks
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,15 +1,17 @@
 class Order < ApplicationRecord
   belongs_to :customer
-  belongs_to :item
-
-  has_many :order_details, dependent: :destroy
+  has_many :order_details
   attachment :image
 
   enum payment: { credit_card: 0, transfer: 1 }
-
+  
   def order_addresses
     Order.select(:postal_code, :address, :name)
   end
+  
+  enum order_addresses: { current_addresses: 0, select_addresses: 1, new_addresses: 2 } 
 
+  
+ 
   validates :name, :postal_code, :address, presence: true
 end
