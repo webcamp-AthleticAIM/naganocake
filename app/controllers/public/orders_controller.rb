04a5a953dfc39b53a629b13a6da0c5e771797e1f
EC2 Default User <ec2-user@ip-172-31-91-106.ec2.internal>
2022-02-22 09:39:55 +0000

    [fix]confirmアクション修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,24 +4,28 @@ class Public::OrdersController < ApplicationController
     @order = Order.new
     @order.customer_id = current_customer.id
     @current_addresses = Customer.select(:postal_code, :address, :last_name, :first_name)
-    @order.name = current_customer.last_name + current_customer.first_name
+    @address = Address.new
 
   end
 
   def confirm
-    @order = Order.new(order_params)
+    p @order = Order.new(order_params)
+    p params[:order][:address_id]
     #@cart_items = CartItem.find(params[:id])
-    if params[:order_addresses] == '0'
-      @order.postal_code = current_customer.postal_code
+    #配送先の選択肢によって動作を分岐
+    if params[:order][:order_addresses] == '0'
+      p @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
       @order.name = current_customer.first_name + current_customer.last_name
-    elsif params[:order_addresses] == '1'
-      @order = Order.new(order_params)
-      @address = Address.find(params[:id])
-      @order.postal_code = @address.postal_code
-      @order.address = @address.address
-      @order.name = @address.name
-    elsif params[:order_addresses] == '2' && @order.postal_code? && @order.address? && @order.name?
+    elsif params[:order][:order_addresses] == '1'
+      if p Address.exists?(id: params[:order][:address_id])
+        p @order.name = Address.find(params[:order][:address_id]).name
+        @order.address = Address.find(params[:order][:address_id]).address
+        @order.postal_code = Address.find(params[:order][:address_id]).postal_code
+      else
+        render 'new'
+      end
+    elsif params[:order][:order_addresses] == '2' && @order.postal_code? && @order.address? && @order.name?
 
     else
       render 'new'
@@ -29,12 +33,23 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.find(order_params)
-
+    #@order = Order.find(order_params)
+    @cart_items = current_customer.cart_items.all
+    @order = current_customer.orders.new(order_params)
     if @order.save
+
+      #注文詳細に保存
+      order_detail = OrderItem.new
+      order_detail.item_id = cart_item.item_id
+      order_detail.order_id = @order.id
+      order_detail.item_quantity = cart_item.item_amount
+      order_detail.price = cart_item.item.price
+      order_detail.save
+      #カート内商品を空にする
       current_customer.cart_items.destroy_all
       redirect_to 'thanks'
     else
+      @order = Order.new(order_params)
       render 'new'
     end
 
@@ -54,5 +69,9 @@ class Public::OrdersController < ApplicationController
 
   def order_params
     params.require(:order).permit(:customer_id, :name, :postal_code, :address, :payment, :postage, :total_payment, :order_status)
+  end
+
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name)
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,17 +1,13 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details
+  has_many :addresses
   attachment :image
 
   enum payment: { credit_card: 0, transfer: 1 }
-  
-  def order_addresses
-    Order.select(:postal_code, :address, :name)
-  end
-  
-  enum order_addresses: { current_addresses: 0, select_addresses: 1, new_addresses: 2 } 
 
-  
- 
+
+
+
   validates :name, :postal_code, :address, presence: true
 end
