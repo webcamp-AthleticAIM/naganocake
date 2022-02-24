a648cfa7dffc925f837453d33afd429a66e4b317
EC2 Default User <ec2-user@ip-172-31-3-84.ec2.internal>
2022-02-24 03:19:04 +0000

    [Fix] 顧客ログイン・ログアウト後の遷移先についての記述を修正

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -8,9 +8,7 @@ class Public::SessionsController < Devise::SessionsController
     customers_mypage_path
   end
 
-  def after_sign_out_path_for(resource)
-    root_path
-  end
+
   # GET /resource/sign_in
   # def new
   #   super
